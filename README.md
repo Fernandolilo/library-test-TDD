0b3faa2d9e41cfc62b8caace3fbdc595c486e643
Fernandolilo <fernando.sistenpro@outlook.com>
2023-06-12 10:22:02 -0300

    Escopo de test para novo book

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# library-test-TDD
+		
+		//mapeamento do arq.json no caso ainda esta null devido não existir ainda a class book
+		String json = new ObjectMapper().writeValueAsString(null);
+		
+		//scopo de post new book
+		MockHttpServletRequestBuilder requestBuilder =
+		MockMvcRequestBuilders
+				.post(BOOL_API)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.content(json);
+
+		mvc
+			.perform(requestBuilder)
+			.andExpect(MockMvcResultMatchers.status().isCreated() )
+			.andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty() )
+			.andExpect(MockMvcResultMatchers.jsonPath("title").value("Meu livro") )
+			.andExpect(MockMvcResultMatchers.jsonPath("autor").value("Fernando") )
+			.andExpect(MockMvcResultMatchers.jsonPath("isbn").value("123123") )
+			;
+	}
+
+
+
+Criado o escopo de test, ele não passou devido não existir uma rota ou seja um path,
+proximo passo temos que criar um controller.Book com o mapeamento para books
+desta forma esta test irá passar com sucesso!
+
+ library-test-TDDMockHttpServletRequest:
+      HTTP Method = POST
+      Request URI = /books
+       Parameters = {}
+          Headers = [Content-Type:"application/json;charset=UTF-8", Accept:"application/json", Content-Length:"4"]
+             Body = null
+    Session Attrs = {}
+
+Handler:
+             Type = org.springframework.web.servlet.resource.ResourceHttpRequestHandler
+
+Async:
+    Async started = false
+     Async result = null
+
+Resolved Exception:
+             Type = null
+
+ModelAndView:
+        View name = null
+             View = null
+            Model = null
+
+FlashMap:
+       Attributes = null
+
+MockHttpServletResponse:
+           Status = 404
+    Error message = null
+          Headers = [Vary:"Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"]
+     Content type = null
+             Body = 
+    Forwarded URL = null
+   Redirected URL = null
+          Cookies = []
--- src/test/java/com/systempro/library/controllerTest/LibraryControllerTests.java
+++ src/test/java/com/systempro/library/controllerTest/LibraryControllerTests.java
@@ -3,10 +3,18 @@ package com.systempro.library.controllerTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -14,10 +22,41 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @AutoConfigureMockMvc
 public class LibraryControllerTests {
 
+	static String BOOL_API = "/books";
+
+	@Autowired
+	MockMvc mvc;
+
 	@Test
 	@DisplayName("Post new book")
-	void createBoolTest() {
+	void createBookTest() throws Exception {
+
 		
+		//metodo para gerar um json
+		String json = new ObjectMapper().writeValueAsString(null);
+		
+		//scopo de post new book
+		MockHttpServletRequestBuilder requestBuilder =
+		MockMvcRequestBuilders
+				.post(BOOL_API)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.content(json);
+
+		mvc
+			.perform(requestBuilder)
+			.andExpect(MockMvcResultMatchers.status().isCreated() )
+			.andExpect(MockMvcResultMatchers.jsonPath("id").isNotEmpty() )
+			.andExpect(MockMvcResultMatchers.jsonPath("title").value("Meu livro") )
+			.andExpect(MockMvcResultMatchers.jsonPath("autor").value("Fernando") )
+			.andExpect(MockMvcResultMatchers.jsonPath("isbn").value("123123") )
+			;
 	}
-	
+
+	@Test
+	@DisplayName("Error new book")
+	void createInvalidBookTest() {
+
+	}
+
 }
