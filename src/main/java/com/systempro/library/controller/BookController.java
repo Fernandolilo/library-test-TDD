d2225e85203248806f62ce34b3efcaf3407c2533
Fernandolilo <fernando.sistenpro@outlook.com>
2023-06-13 07:51:47 -0300

    add ModelMapper na app, refatorado o cod esta mais clean

--- src/main/java/com/systempro/library/controller/BookController.java
+++ src/main/java/com/systempro/library/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.systempro.library.controller;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,30 +17,23 @@ import com.systempro.library.service.BookService;
 public class BookController {
 
 	private final BookService service;
-
-	public BookController(BookService service) {
+	private final ModelMapper modelMapper;
+	
+	public BookController(BookService service,  ModelMapper modelMapper) {
+		this.modelMapper = modelMapper;
 		this.service = service;
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public BookDTO create(@RequestBody BookDTO request) {
-		Book entity = 
-				Book.builder()				
-				.autor(request.getAutor())
-				.title(request.getTitle())
-				.isbn(request.getIsbn())
-				.build();
+		Book entity = modelMapper.map(request, Book.class);
+				
 				
 		entity = service.save(entity);
 	  
 
-		return BookDTO.builder()	
-				.id(entity.getId())
-				.autor(entity.getAutor())
-				.title(entity.getTitle())
-				.isbn(entity.getIsbn())
-				.build();
+		return modelMapper.map(entity, BookDTO.class);
 	  
 	}
 
